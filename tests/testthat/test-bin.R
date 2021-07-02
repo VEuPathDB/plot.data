b62cba2e2341183e4265e894045d4b36f0f83c96
asizemore <annsize@seas.upenn.edu>
2021-07-02 17:42:19 -0400

    added barmode option to binProportion

--- tests/testthat/test-bin.R
+++ tests/testthat/test-bin.R
@@ -3,22 +3,22 @@ context('bin')
 test_that("binProportion() returns an appropriately sized data.table", {
   viewport <- list('xMin'=min(data.xy$y), 'xMax'=max(data.xy$y)) 
 
-  dt <- binProportion(data.xy,'y', binWidth=.1, viewport=viewport)
+  dt <- binProportion(data.xy,'y', binWidth=.1, barmode='stack', viewport=viewport)
   expect_is(dt, 'data.table')
   expect_equal(nrow(dt),1)
   expect_equal(names(dt), c('binLabel', 'binStart', 'binEnd', 'value'))
 
-  dt <- binProportion(data.xy, 'y','group', binWidth=.1, viewport=viewport)
+  dt <- binProportion(data.xy, 'y','group', binWidth=.1, barmode='stack', viewport=viewport)
   expect_is(dt, 'data.table')
   expect_equal(nrow(dt),4)
   expect_equal(names(dt), c('group', 'binLabel', 'binStart', 'binEnd', 'value'))
 
-  dt <- binProportion(data.xy, 'y', NULL, 'panel', binWidth=.1, viewport=viewport)
+  dt <- binProportion(data.xy, 'y', NULL, 'panel', binWidth=.1, barmode = 'stack', viewport=viewport)
   expect_is(dt, 'data.table')
   expect_equal(nrow(dt),4)
   expect_equal(names(dt), c('panel', 'binLabel', 'binStart', 'binEnd', 'value'))
 
-  dt <- binProportion(data.xy, 'y', 'group', 'panel', binWidth=.1, viewport=viewport)
+  dt <- binProportion(data.xy, 'y', 'group', 'panel', binWidth=.1, barmode = 'stack', viewport=viewport)
   expect_is(dt, 'data.table')
   expect_equal(nrow(dt),16)
   expect_equal(names(dt), c('group', 'panel', 'binLabel', 'binStart', 'binEnd', 'value'))
