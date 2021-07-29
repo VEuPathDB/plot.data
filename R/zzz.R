cf1c500fbee63e2fa7363bbee17fcb18fa615b9e
asizemore <annsize@seas.upenn.edu>
2021-07-29 17:10:32 -0400

    set env var with load_dot_env

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-## On load, run the following
-.onLoad <- function(libname, pkgname) {
-  Sys.setenv(TZ='EST')
-  packageStartupMessage("plot.data loaded. Happy plotting!")
-  invisible()
-}
-
-# on.Unload() to remove side effects.
