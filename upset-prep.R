fea3739e7b0dbf7df739d81aa6d8dace1dec9563
asizemore <annsize@seas.upenn.edu>
2021-08-02 10:36:36 -0400

    calculate intersection of missingness, not completeness

--- upset-prep.R
+++ upset-prep.R
@@ -14,8 +14,10 @@ df$panel[sample(1:500, 200, replace=F)] <- NA
 
 
 # Get combinations in the upset format
-# Dumb implementation - worth improving later
+# Dumb implementation - worth improving later for speed
 # Also worth validating against upsetR
+
+## Assume we implement missingness intersections, not completeness
 getUpsetIntersections <- function(set, df, type) {
   
   if (type == 'intersection') {
@@ -24,20 +26,22 @@ getUpsetIntersections <- function(set, df, type) {
     
   } else if (type=='distinctIntersection') {
     
-    # Remove all rows which have values in columns not in the set
+    # Remove all rows which are missing data in any column not in the set
     notset <- setdiff(colnames(df), set)
     if (length(notset) > 0){
-      subsetDf <- df[rowSums(is.na(df[, ..notset])) == length(notset), ..set]
+      # subsetDf <- df[rowSums(!is.na(df[, ..notset])) == length(notset), ..set]
+      subsetDf <- df[complete.cases(df[, ..notset]), ..set]
     } else {
       subsetDf <- df
     }
     
   }
   
-  # Now want complete cases of df
-  ncomplete <- sum(complete.cases(subsetDf))
+  # Now want number of all missing
+  nMissing <- nrow(subsetDf[rowSums(is.na(subsetDf[, ..set])) == length(set), ])
+  
   return (list("name" = paste(set,collapse=" n ")
-               , "cardinality" = ncomplete
+               , "cardinality" = nMissing
                , "degree" = length(set)
                , "elems" = c()
                , "sets" = 'Set'
@@ -45,12 +49,33 @@ getUpsetIntersections <- function(set, df, type) {
 } # End function
 
 
-
+# Calculate power set of vars
 pSet <- rje::powerSet(colnames(df))
 
 # Remove empty set
 pSet <- pSet[lapply(pSet, length) > 0]
 
+# Find intersections
 pSet_intersection <- lapply(pSet, getUpsetIntersections, df=df, type='intersection')
 pSet_distinctIntersection <- lapply(pSet, getUpsetIntersections, df=df, type='distinctIntersection')
 
+
+## Testing
+df <- data.xy
+na_locs <- sample(1:500, 100)
+df[na_locs, c('group','x')] <- NA
+df[na_locs[1:50], c('panel')] <- NA
+pSet <- rje::powerSet(colnames(df))
+# Remove empty set
+pSet <- pSet[lapply(pSet, length) > 0]
+
+# Compute intersections and check outputs
+pSet_intersection <- lapply(pSet, getUpsetIntersections, df=df, type='intersection')
+print(pSet_intersection[[9]]$cardinality == 100) # group n x
+print(pSet_intersection[[2]]$cardinality == 50) # panel
+print(pSet_intersection[[11]]$cardinality == 50) # group n panel n x
+
+pSet_distinctIntersection <- lapply(pSet, getUpsetIntersections, df=df, type='distinctIntersection')
+print(pSet_distinctIntersection[[9]]$cardinality == 50) # group n x
+print(pSet_distinctIntersection[[2]]$cardinality == 0) # panel
+print(pSet_distinctIntersection[[11]]$cardinality == 50) # group n panel n x
