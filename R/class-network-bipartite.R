730d8a7458e1f2463f1a07412d06cd72ce4ccbb3
asizemore <annsize@seas.upenn.edu>
2023-10-19 13:57:12 -0400

    clean bipartite network docs

--- R/class-network-bipartite.R
+++ R/class-network-bipartite.R
@@ -7,14 +7,11 @@ newBipartiteNetwork <- function(df = data.frame(),
                                 verbose = logical()
 ) {
 
-  # Assume the source nodes are column 1, and the target are all in column 2.
+  # Assume the source nodes are column 1, and the target are column 2.
   column1NodeIDs <- sort(unique(df[[sourceNodeColumn]]))
   column2NodeIDs <- sort(unique(df[[targetNodeColumn]]))
 
-  # TODO check that no nodes are in both cols
-
-  # Create a basic network. We'll threshold the edges in here so we want to know about all the nodes 
-  # before that happens. (add boolean removeIsolatedNodes and linkWeightThreshold fn i guess...)
+  # Create a network
   net <- newNetwork(df = df,
                     sourceNodeColumn = sourceNodeColumn,
                     targetNodeColumn = targetNodeColumn,
@@ -25,6 +22,7 @@ newBipartiteNetwork <- function(df = data.frame(),
                     class = 'bipartite'
   )
 
+  # Add bipartite network attributes
   attr <- attributes(net)
   attr$column1NodeIDs <- column1NodeIDs
   attr$column2NodeIDs <- column2NodeIDs
@@ -38,6 +36,13 @@ newBipartiteNetwork <- function(df = data.frame(),
 
 validateBipartiteNetwork <- function(bpnet, verbose) {
 
+  networkAttributes <- attributes(bpnet)
+  
+  # Ensure no node is in both columns
+  if (any(networkAttributes$column1NodeIDs %in% networkAttributes$column2NodeIDs)) {
+    stop('Nodes cannot reside in both columns of a bipartite network')
+  }
+
   class <- attr(bpnet, 'class')
   stopifnot(is.character(class))
 
@@ -46,12 +51,25 @@ validateBipartiteNetwork <- function(bpnet, verbose) {
   return(net)
 }
 
+
+
 #' Create bipartite network
 #'
-#' This function returns the name of a json file which it has
-#' written a data.table object out to.
-#' @param .pd a data.table to convert to json and write to a tmp file
-#' @param pattern optional tmp file prefix
+#' This function creates an object that represents a bipartite network. A bipartite network is a network
+#' in which there are two groups (columns) of nodes, and links only connect nodes in separate groups (columns).
+#' @param df a data.frame of links, with one row per link. One column should contain the link source node, and one
+#' column should contain the link target node. It will be assumed that all nodes in the source column are in one group, 
+#' and that all nodes in the target column are in the second group.
+#' @param sourceNodeColumn string defining the name of the column in df that corresponds to the source nodes
+#' @param targetNodeColumn string defining the name of the column in df that corresponds to the target nodes
+#' @param linkWeightColumn optional string defining the name of the column in df that corresponds to the weight of the link
+#' @param nodeIDs optional string array. Should contain at least all nodes in the sourceNodeColumn and targetNodeColumn of df.
+#' May also include nodes that do not have any links (isolated nodes). If not specified, the complete list of network nodes
+#' will be the union of those in the sourceNodeColumn and targetNodeColumn.
+#' @param linkColorScheme string denoting the type of coloring scheme to apply to edges. Options are 'none' (default) which
+#' which does not attempt to assign a color to links. The 'posneg' option assigns the link color to the sign of the linkWeightColumn.
+#' If linkColorSheme is not 'none', a linkWeightColumn must be specified.
+#' @param verbose boolean to determine if time-stamped logging is desired.
 #' @return bipartite network
 #' @export
 bipartiteNetwork <- function(df = data.frame(),
@@ -73,7 +91,6 @@ bipartiteNetwork <- function(df = data.frame(),
   )
 
   return(bpnet)
-  # outFile <- writeNetworkToJSON(bpnet, 'bipartiteNetwork', verbose=verbose) No, this just goes in the plugin
 
 }
 
