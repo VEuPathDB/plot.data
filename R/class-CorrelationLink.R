b78e65c0d1fce9fa04afb8f66f7a566c0fac40f0
Danielle Callan <dcallan@upenn.edu>
2024-04-24 11:16:47 -0400

    add dedicated class for links representing correlation results

---
+++ R/class-CorrelationLink.R
@@ -0,0 +1,240 @@
+
+
+check_correlation_link <- function(object) {
+
+  errors <- character()
+
+  # check correlation coef (weight) and pvalue make some sense
+
+  return(if (length(errors) == 0) TRUE else errors) 
+}
+
+#' CorrelationLink
+#' 
+#' Represent one singular link in a correltion network. A link has a source,
+#' a target, a correlation coefficient (weight), and a p-value. It is undirected.
+#' It may have a color, timestamp, or label (coming soon!)
+#' 
+#' @name CorrelationLink-class
+#' @rdname CorrelationLink-class
+#' @include class-Node.R
+#' @include constructors-Node.R
+#' @include class-Link.R
+#' @export
+setClass("CorrelationLink", 
+  slots = list(
+    pValue = "numeric"
+  ),
+  prototype = prototype(
+    pValue = NULL,
+    weight = NULL, # change default of 1 from parent Link class
+  ),
+  validity = check_correlation_link
+)
+
+#' CorrelationLink constructor
+#' 
+#' @param source The source node identifier
+#' @param target The target node identifier
+#' @param correlationCoef The correlation coefficient (weight) of the link
+#' @param pValue The p-value of the link
+#' @param color The color of the link
+#' @export
+#' @rdname CorrelationLink
+setGeneric("CorrelationLink", 
+function(
+  source, 
+  target, 
+  correlationCoef = 1, 
+  pValue = NULL, 
+  color = NULL
+) standardGeneric("CorrelationLink"), 
+signature = c("source", "target"))
+
+#' @rdname CorrelationLink
+#' @aliases CorrelationLink,Node,Node-method
+setMethod("CorrelationLink", c("Node", "Node"), function(source, target, correlationCoef, pValue, color = NULL) {
+  new("CorrelationLink", source = NodeId(id(source)), target = NodeId(id(target)), weight = correlationCoef, pValue = pValue, color = color)
+})
+
+#' @rdname CorrelationLink
+#' @aliases CorrelationLink,character,character-method
+setMethod("CorrelationLink", c("character", "character"), function(source, target, correlationCoef, pValue, color = NULL) {
+  CorrelationLink(source = NodeId(source), target = NodeId(target), weight = correlationCoef, pValue = pValue, color = color)
+})
+
+#' @rdname CorrelationLink
+#' @aliases CorrelationLink,numeric,numeric-method
+setMethod("CorrelationLink", c("numeric", "numeric"), function(source, target, correlationCoef, pValue, color = NULL) {
+  CorrelationLink(source = NodeId(source), target = NodeId(target), weight = correlationCoef, pValue = pValue, color = color)
+})
+
+#' @rdname CorrelationLink
+#' @aliases CorrelationLink,NodeId,NodeId-method
+setMethod("CorrelationLink", c("NodeId", "NodeId"), function(source, target, correlationCoef, pValue, color = NULL) {
+  new("CorrelationLink", source = source, target = target, weight = correlationCoef, pValue = pValue, color = color)
+})
+
+#' @rdname CorrelationLink
+#' @aliases CorrelationLink,missing,missing-method
+setMethod("CorrelationLink", c("missing", "missing"), function(source, target, correlationCoef, pValue, color = NULL) {
+  new("CorrelationLink")
+})
+
+# TODO refactor these checks into a helper fxn that any flavor of LinkList can use
+check_correlation_link_list <- function(object) {
+
+  errors <- character()
+  
+  # TODO update these checks for correlation specific stuffs
+  if (any(unlist(lapply(object, function(x) {!is.null(color(x))})))) {
+    # If one link has a color, all must have colors
+    if (!all(unlist(lapply(object, function(x) {!is.null(color(x))})))) {
+      errors <- c(errors, "If one link has a color, all links must have a color")
+    }
+    
+    # Link colors must be all the same class
+    if (length(unique(unlist(lapply(object, function(x) {class(color(x))})))) > 1) {
+      errors <- c(errors, "Link colors must be all the same class")
+    }
+  }
+
+
+  # If one link has a weight, all must have weights
+  if (any(unlist(lapply(object, function(x) {!is.null(weight(x))})))) {
+    if (!all(unlist(lapply(object, function(x) {!is.null(weight(x))})))) {
+      errors <- c(errors, "If one link has a weight, all links must have a weight")
+    }
+  }
+
+
+  return(if (length(errors) == 0) TRUE else errors) 
+
+}
+
+#' Correlation Link List
+#' 
+#' A class for representing links in a correlation network
+#' 
+#' @name CorrelationLinkList-class
+#' @rdname CorrelationLinkList-class
+#' @importFrom S4Vectors SimpleList
+#' @export
+setClass("CorrelationLinkList", 
+  contains = "SimpleList",
+  prototype = prototype(
+    elementType = "CorrelationLink"
+  ),
+  validity = check_correlation_link_list
+)
+
+#' Generate a CorrelationLinkList
+#' 
+#' Generate a CorrelationLinkList from an edgeList
+#' @param object Object containing data to be converted to a LinkList. Could be a SimpleList of Links or a data.frame
+#' with columns source, target, correlationCoef, pValue.
+#' @param linkColorScheme Either 'none' or 'posneg'. If 'posneg', the link color will be based on the sign of the correlation coefficient.
+#' @param correlationCoefThreshold numeric value used to filter links based on correlationCoef. Default is NULL (i.e. no filtering).
+#' Any links with an absolute correlationCoef below this threshold will be removed.
+#' @param pValueThreshold numeric value used to filter links based on pValue. Default is 0.05.
+#' Any links with an pValue above this threshold will be removed.
+#' @return CorrelationLinkList
+#' @export
+#' @examples
+#' CorrelationLinkList(data.frame(source='a',target='b',correlationCoef=0.5,pValue=0.01))
+#' @rdname CorrelationLinkList
+setGeneric("CorrelationLinkList", 
+function(
+  object, 
+  linkColorScheme = c('none', 'posneg'),
+  correlationCoefThreshold = NULL,
+  pValueThreshold = 0.05  
+) standardGeneric("CorrelationLinkList"), signature = c("object"))
+
+#' @rdname CorrelationLinkList
+#' @aliases CorrelationLinkList,data.frame-method
+setMethod("CorrelationLinkList", "data.frame", 
+function(
+  object = data.frame(
+    source=character(),
+    target=character(),
+    correlationCoef=numeric(),
+    pValue=numeric()
+  ), 
+  linkColorScheme = c('none', 'posneg'),
+  correlationCoefThreshold = NULL,
+  pValueThreshold = 0.05
+) {
+  linkColorScheme <- veupathUtils::matchArg(linkColorScheme)
+  
+  if (!inherits(isValidEdgeList(object), "logical")) {
+    stop(paste("Invalid edgeList:", isValidEdgeList(object), collapse = '\n'))
+  }
+
+  ## additional validation of correlationCoef and pValue in edgeList
+  if (!all(c('correlationCoef', 'pValue') %in% colnames(object))) {
+    stop("edgeList must contain columns 'correlationCoef' and 'pValue' for CorrelationLinkList/ CorrelationNetwork")
+  }
+
+  ## remove rows w NA correlationCoef, or not meeting thresholds
+  object <- object[!is.na(object$correlationCoef), ]
+
+  if (!is.null(correlationCoefThreshold)) {
+    object <- object[abs(object$correlationCoef) >= correlationCoefThreshold, ]
+  }
+  if (!is.null(pValueThreshold)) {
+    object <- object[object$pValue <= pValueThreshold, ]
+  }
+
+  if (nrow(object) == 0) {
+    new("CorrelationLinkList")
+  }
+
+  makeLink <- function(x, linkColorScheme) {
+    source <- unname(x['source'])
+    target <- unname(x['target'])
+    correlationCoef <- as.numeric(unname(x['correlationCoef']))
+    pValue <- as.numeric(unname(x['pValue']))
+
+    if (linkColorScheme == 'posneg') {
+      if (correlationCoef < 0) {
+        color <- -1
+      } else if (correlationCoef > 0) {
+        color <- 1
+      } else {
+        color <- 0
+      }
+    } else {
+      color <- NA_character_
+    }
+    
+    if (is.na(color)) {
+      link <- CorrelationLink(source, target, correlationCoef, pValue, NULL)
+    } else {
+      link <- CorrelationLink(source, target, correlationCoef, pValue, color)
+    }
+
+    return(link)
+  }
+
+  linkList <- apply(object, 1, makeLink, linkColorScheme)
+  new("CorrelationLinkList", linkList)
+})
+
+#' @rdname
+#' @aliases CorrelationLinkList,missing-method
+setMethod("CorrelationLinkList", "missing", function(object) {
+  new("CorrelationLinkList")
+})
+
+#' @rdname
+#' @aliases CorrelationLinkList,SimpleList-method 
+setMethod("CorrelationLinkList", "SimpleList", function(object) {
+  new("CorrelationLinkList", object)
+})
+
+#' @rdname
+#' @aliases CorrelationLinkList,list-method
+setMethod("CorrelationLinkList", "list", function(object) {
+  new("CorrelationLinkList", object)
+})
---
+++ R/methods-CorrelationLinks.R
@@ -0,0 +1,64 @@
+# Methods for Link and LinkList objects
+
+# Accessors for fanciness
+setGeneric("correlationCoef", function(object) standardGeneric("correlationCoef"))
+setGeneric("correlationCoef<-", function(object, value) standardGeneric("correlationCoef<-"))
+setGeneric("pValue", function(object) standardGeneric("pValue"))
+setGeneric("pValue<-", function(object, value) standardGeneric("pValue<-"))
+
+setMethod("correlationCoef", "CorrelationLink", function(object) object@correlationCoef)
+setMethod("correlationCoef<-", "CorrelationLink", function(object, value) {object@correlationCoef <- value; validObject(object); object})
+setMethod("pValue", "CorrelationLink", function(object) object@pValue)
+setMethod("pValue<-", "CorrelationLink", function(object, value) {object@pValue <- value; validObject(object); object})
+
+# a method to filter CorrelationLinkList by pValue and correlationCoef
+#' Filter Correlation Links
+#' 
+#' This function filters CorrelationLinkList by pValue and correlationCoef
+#' @param object CorrelationLinkList or CorrelationNetwork
+#' @param correlationCoefThrehold threshold to filter edges by correlation coefficient. Default is NULL.
+#' Any links with absolute correlation coefficients below this threshold will be removed.
+#' @param pValueThreshold threshold to filter edges by p-value. Default is 0.05.
+#' Any links with p-values above this threshold will be removed.
+#' @param verbose boolean indicating if timed logging is desired
+#' @return CorrelationLinkList or CorrelationNetwork
+#' @export
+#' @rdname pruneCorrelationLinks
+setGeneric("pruneCorrelationLinks", 
+function(
+    object, 
+    correlationCoefThrehold = NULL,
+    pValueThreshold = 0.05, 
+    verbose = c(TRUE, FALSE)
+) {
+    standardGeneric("pruneCorrelationLinks")
+})
+
+#' @rdname pruneCorrelationLinks
+#' @aliases pruneCorrelationLinks,CorrelationLinkList-method
+setMethod("pruneCorrelationLinks", "CorrelationLinkList", 
+function(
+    object, 
+    correlationCoefThreshold = NULL,
+    pValueThreshold = 0.05,
+    verbose = c(TRUE, FALSE)
+) {
+    verbose <- veupathUtils::matchArg(verbose)
+
+    if (!is.null(correlationCoefThreshold)) {
+        correlationCoefs <- sapply(object, correlationCoef)
+        newLinks <- object[abs(correlationCoefs) >= correlationCoefThreshold]
+    }
+
+    if (!is.null(pValueThreshold)) {
+        pValues <- sapply(object, pValue)
+        newLinks <- newLinks[pValues <= pValueThreshold]
+    }
+
+    if (verbose) {
+        message("Removed ", length(object) - length(newLinks), " links")
+    }
+
+    validObject(newLinks)
+    return(newLinks)
+})
